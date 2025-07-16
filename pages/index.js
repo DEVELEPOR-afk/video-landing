1c5023ecfda00deeb2cbb9804958876f505aac63
SOUMO <soumo2020.saha@gmail.com>
2025-07-16 18:39:19 +0530

    l

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+// Temporary pages router file to satisfy Vercel routes-manifest generation.
+// It simply re-exports the main App Home page implemented in the App Router.
+export { default } from "../src/app/page";
